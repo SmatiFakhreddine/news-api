a70b6b060d0dbf86c05828777771c9a767e8a633
SmatiFakhreddine <smatifakhri9@gmail.com>
2025-06-06 00:45:40 +0100

    Set Explicit Timeouts

--- Server/backendServer.js
+++ Server/backendServer.js
@@ -8,6 +8,11 @@ env.config();
 const port = process.env.PORT;
 const API_URL = process.env.API_URL;
 
+const apiClient = axios.create({
+  timeout: 3000,
+  timeoutErrorMessage: 'External API request timed out'
+});
+
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,7 +21,7 @@ app.use(bodyParser.json());
 // Route to render the main page
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get(`${API_URL}/posts`);
+    const response = await apiClient.get(`${API_URL}/posts`);
     console.log(response.data);
     res.render("index.ejs", { posts: response.data });
   } catch (error) {
@@ -31,7 +36,7 @@ app.get("/new", (req, res) => {
 
 app.get("/edit/:id", async (req, res) => {
   try {
-    const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
+    const response = await apiClient.get(`${API_URL}/posts/${req.params.id}`);
     console.log(response.data);
     res.render("modify.ejs", {
       heading: "Edit Post",
@@ -46,7 +51,7 @@ app.get("/edit/:id", async (req, res) => {
 // Create a new post
 app.post("/api/posts", async (req, res) => {
   try {
-    const response = await axios.post(`${API_URL}/posts`, req.body);
+    const response = await apiClient.post(`${API_URL}/posts`, req.body);
     console.log(response.data);
     res.redirect("/");
   } catch (error) {
@@ -58,7 +63,7 @@ app.post("/api/posts", async (req, res) => {
 app.post("/api/posts/:id", async (req, res) => {
   console.log("called");
   try {
-    const response = await axios.patch(`${API_URL}/posts/${req.params.id}`, req.body);
+    const response = await apiClient.patch(`${API_URL}/posts/${req.params.id}`, req.body);
     console.log(response.data);
     res.redirect("/");
   } catch (error) {
@@ -69,7 +74,7 @@ app.post("/api/posts/:id", async (req, res) => {
 // Delete a post
 app.get("/api/posts/delete/:id", async (req, res) => {
   try {
-    await axios.delete(`${API_URL}/posts/${req.params.id}`);
+    await apiClient.delete(`${API_URL}/posts/${req.params.id}`);
     res.redirect("/");
   } catch (error) {
     res.status(500).json({ message: "Error deleting post" });
